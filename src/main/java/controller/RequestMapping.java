4a27c46748afc826f68679352956134d304391a7
sondahyun <88223753+sondahyun@users.noreply.github.com>
2022-11-12 09:51:03 +0900

    Update RequestMapping.java

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -10,35 +10,43 @@ import controller.user.*;
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     
-    // �� ��û uri�� ���� controller ��ü�� ������ HashMap ����
+    // 각 요청 uri에 대한 controller 객체를 저장할 HashMap 생성
     private Map<String, Controller> mappings = new HashMap<String, Controller>();
 
     public void initMapping() {
-    	// �� uri�� �����Ǵ� controller ��ü�� ���� �� ����
+    	// 각 uri에 대응되는 controller 객체를 생성 및 저장
         mappings.put("/", new ForwardController("index.jsp"));
         mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
         mappings.put("/user/login", new LoginController());
         mappings.put("/user/logout", new LogoutController());
+//        mappings.put("/user/list", new ListUserController());
         mappings.put("/user/view", new ViewUserController());
         
-        // ȸ�� ���� �� ��û�� ���� ��û ó�� ���� (���� Ŀ�´�Ƽ ���� �޴� �߰��� ����)
+        // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
 //      mappings.put("/user/register/form", new ForwardController("/user/registerForm.jsp"));
 //      mappings.put("/user/register", new RegisterUserController());
         mappings.put("/user/register", new RegisterUserController());
 
-        // ����� ���� ���� �� ��û�� ���� ��û ó�� ����
+        // 사용자 정보 수정 폼 요청과 수정 요청 처리 병합
 //      mappings.put("/user/update/form", new UpdateUserFormController());
 //      mappings.put("/user/update", new UpdateUserController());        
         mappings.put("/user/update", new UpdateUserController());
         
         mappings.put("/user/delete", new DeleteUserController());
         
-       
+		/*
+		 * // 커뮤니티 관련 request URI 추가 mappings.put("/community/list", new
+		 * ListCommunityController()); mappings.put("/community/view", new
+		 * ViewCommunityController()); mappings.put("/community/create/form", new
+		 * ForwardController("/community/creationForm.jsp"));
+		 * mappings.put("/community/create", new CreateCommunityController());
+		 * mappings.put("/community/update", new UpdateCommunityController());
+		 */
         logger.info("Initialized Request Mapping!");
     }
 
     public Controller findController(String uri) {	
-    	// �־��� uri�� �����Ǵ� controller ��ü�� ã�� ��ȯ
+    	// 주어진 uri에 대응되는 controller 객체를 찾아 반환
         return mappings.get(uri);
     }
 }
