a0f6b7c60c5d3c2e56302aa07858ae0cc57ce504
sondahyun <88223753+sondahyun@users.noreply.github.com>
2022-11-12 10:00:09 +0900

    n

--- src/main/java/model/dao/UserDAO.java
+++ src/main/java/model/dao/UserDAO.java
@@ -7,77 +7,77 @@ import java.util.List;
 import model.UserInfo;
 
 /**
- * ����� ������ ���� �����ͺ��̽� �۾��� �����ϴ� DAO Ŭ���� USERINFO ���̺� �����
- * ������ �߰�, ����, ����, �˻� ����
+ * 사용자 관리를 위해 데이터베이스 작업을 전담하는 DAO 클래스
+ * USERINFO 테이블에 사용자 정보를 추가, 수정, 삭제, 검색 수행 
  */
 public class UserDAO {
 	private JDBCUtil jdbcUtil = null;
 
 	public UserDAO() {
-		jdbcUtil = new JDBCUtil(); // JDBCUtil ��ü ����
+		jdbcUtil = new JDBCUtil(); // JDBCUtil 객체 생성
 	}
 
 	/**
-	 * ����� ���� ���̺� ���ο� ����� ����.
+	 * 사용자 관리 테이블에 새로운 사용자 생성.
 	 */
 	public int create(UserInfo user) throws SQLException {
 		String sql = "INSERT INTO UserInfo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		Object[] param = new Object[] { user.getUserId(), user.getLoginId(), user.getLoginPwd(), user.getUserNickname(),
 				user.getUserBirth(), user.getPhoneNumber(), user.getGender(), user.getAddress(), user.getPetList() };
-		jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil �� insert���� �Ű� ���� ����
+		jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil 에 insert문과 매개 변수 설정
 
 		try {
-			int result = jdbcUtil.executeUpdate(); // insert �� ����
+			int result = jdbcUtil.executeUpdate(); // insert 문 실행
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		} finally {
 			jdbcUtil.commit();
-			jdbcUtil.close(); // resource ��ȯ
+			jdbcUtil.close(); // resource 반환
 		}
 		return 0;
 	}
 
 	/**
-	 * ������ ����� ������ ����.
+	 * 기존의 사용자 정보를 수정.
 	 */
 	public int update(UserInfo user) throws SQLException {
 		String sql = "UPDATE UserInfo "
 				+ "SET loginPwd=?, userNickname=?, userBirth=?, phoneNumber=?, address=?, petList=?" + "WHERE userId=?";
 		Object[] param = new Object[] { user.getLoginPwd(), user.getUserNickname(), user.getUserBirth(),
 				user.getPhoneNumber(), user.getAddress(), user.getPetList(), user.getUserId() };
-		jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil�� update���� �Ű� ���� ����
+		jdbcUtil.setSqlAndParameters(sql, param); // // JDBCUtil에 update문과 매개 변수 설정
 
 		try {
-			int result = jdbcUtil.executeUpdate(); // update �� ����
+			int result = jdbcUtil.executeUpdate(); // update 문 실행
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		} finally {
 			jdbcUtil.commit();
-			jdbcUtil.close(); // resource ��ȯ
+			jdbcUtil.close(); //  resource 반환
 		}
 		return 0;
 	}
 
 	/**
-	 * ����� ID�� �ش��ϴ� ����ڸ� ����.
+	 * 사용자 ID에 해당하는 사용자를 삭제.
 	 */
 	public int remove(String loginId) throws SQLException {
 		String sql = "DELETE FROM UserInfo WHERE loginId=?";
-		jdbcUtil.setSqlAndParameters(sql, new Object[] { loginId }); // JDBCUtil�� delete���� �Ű� ���� ����
+		jdbcUtil.setSqlAndParameters(sql, new Object[] { loginId }); // JDBCUtil에 delete문과 매개 변수 설정
 
 		try {
-			int result = jdbcUtil.executeUpdate(); // delete �� ����
+			int result = jdbcUtil.executeUpdate(); // delete 문 실행
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		} finally {
 			jdbcUtil.commit();
-			jdbcUtil.close(); // resource ��ȯ
+			jdbcUtil.close(); // resource 반환
 		}
 		return 0;
 	}
@@ -85,14 +85,14 @@ public class UserDAO {
 	@SuppressWarnings("unchecked")
 	public UserInfo findUser(String loginId) throws SQLException {
 		String sql = "SELECT * " + "FROM UserInfo" + "WHERE loginId=? ";
-		jdbcUtil.setSqlAndParameters(sql, new Object[] { loginId }); // JDBCUtil �� query문과 ׺�개 � �� �� ��
+		jdbcUtil.setSqlAndParameters(sql, new Object[] { loginId }); // JDBCUtil에 query문과 매개 변수 설정
 
 		try {
-			ResultSet rs = jdbcUtil.executeQuery(); // query �� ��
+			ResultSet rs = jdbcUtil.executeQuery(); // query 실행
 			UserInfo user = null;
-			ArrayList<Integer> list = new ArrayList<>();
-			if(rs.next()) {
-				user = new UserInfo();
+			ArrayList<Integer> list = new ArrayList<>();// User들의 리스트 생성
+			if(rs.next()) {// 학생 정보 발견
+				user = new UserInfo();// User 객체를 생성하여 학생 정보를 저장
 				user.setUserId(rs.getInt("userId"));
 				user.setLoginId(rs.getString("loginId"));
 				user.setLoginPwd(rs.getString("loginPwd"));
@@ -115,14 +115,14 @@ public class UserDAO {
 	}
 
 	/**
-	 * �־��� ����� ID�� �ش��ϴ� ����ڰ� �����ϴ��� �˻�
+	 * 전체 사용자 정보를 검색하여 List에 저장 및 반환
 	 */
 	public boolean existingUser(String loginId) throws SQLException {
 		String sql = "SELECT count(*) FROM UserInfo WHERE loginId=?";
-		jdbcUtil.setSqlAndParameters(sql, new Object[] { loginId }); // JDBCUtil�� query���� �Ű� ���� ����
+		jdbcUtil.setSqlAndParameters(sql, new Object[] { loginId }); // JDBCUtil에 query문 설정
 
 		try {
-			ResultSet rs = jdbcUtil.executeQuery(); // query ����
+			ResultSet rs = jdbcUtil.executeQuery(); // query 실행	
 			if (rs.next()) {
 				int count = rs.getInt(1);
 				return (count == 1 ? true : false);
