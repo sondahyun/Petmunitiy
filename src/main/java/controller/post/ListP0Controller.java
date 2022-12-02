9e48795937a4095e46d1359af51b81d3c560739b
정재운 <jaeun101501@gmail.com>
2022-12-02 16:10:26 +0900

    info_community 경로 수정

--- src/main/java/controller/post/ListP0Controller.java
+++ src/main/java/controller/post/ListP0Controller.java
@@ -16,6 +16,6 @@ public class ListP0Controller implements Controller {
 		
 		// commList 객체를 request에 저장하여 커뮤니티 리스트 화면으로 이동(forwarding)
 		request.setAttribute("p0List", p0List);				
-		return "/community/info_community.jsp";        
+		return "/community/info_community/info_community.jsp";        
     }
 }
