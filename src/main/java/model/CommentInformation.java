60ffe2093c965cfbeaaac498719054a2633f8d81
정재운 <jaeun101501@gmail.com>
2022-12-07 19:22:05 +0900

    comment 중간 작업 중

---
+++ src/main/java/model/CommentInformation.java
@@ -0,0 +1,91 @@
+package model;
+
+import java.util.Date;
+
+public class CommentInformation {
+	private int commentId;
+	private Date commentDate;
+	private String commentContent;
+	private int postId;
+	private int userId;
+	
+	
+	public CommentInformation(int commentId, Date commentDate, String commentContent, int postId, int userId) {
+		super();
+		this.commentId = commentId;
+		this.commentDate = commentDate;
+		this.commentContent = commentContent;
+		this.postId = postId;
+		this.userId = userId;
+	}
+
+
+	public CommentInformation(Date commentDate, String commentContent, int postId, int userId) {
+		super();
+		this.commentDate = commentDate;
+		this.commentContent = commentContent;
+		this.postId = postId;
+		this.userId = userId;
+	}
+
+
+	public int getCommentId() {
+		return commentId;
+	}
+
+
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+
+
+	public Date getCommentDate() {
+		return commentDate;
+	}
+
+
+	public void setCommentDate(Date commentDate) {
+		this.commentDate = commentDate;
+	}
+
+
+	public String getCommentContent() {
+		return commentContent;
+	}
+
+
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+
+
+	public int getPostId() {
+		return postId;
+	}
+
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	@Override
+	public String toString() {
+		return "CommentInformation [commentId=" + commentId + ", commentDate=" + commentDate + ", commentContent="
+				+ commentContent + ", postId=" + postId + ", userId=" + userId + "]";
+	}
+	
+	
+	
+	
+}
