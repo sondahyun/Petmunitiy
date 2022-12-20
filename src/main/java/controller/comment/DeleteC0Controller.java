017435dafa695de29d71ae49ec010ec86cb86b85
최가람 <riverchoi2523@gmail.com>
2022-12-21 03:34:14 +0900

    댓글 삭제 구현 완료

--- src/main/java/controller/comment/DeleteC0Controller.java
+++ src/main/java/controller/comment/DeleteC0Controller.java
@@ -2,6 +2,7 @@ package controller.comment;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,14 +12,17 @@ import model.service.UserManager;
 
 public class DeleteC0Controller implements Controller {
     private static final Logger log = LoggerFactory.getLogger(DeleteC0Controller.class);
-
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
-		int commentId = Integer.parseInt(request.getParameter("commentId"));
+    	System.out.println("c0");
+    	int commentId = Integer.parseInt(request.getParameter("commentId"));
     	log.debug("Delete c0_Id : {}", commentId);
-
+    	
 		UserManager manager = UserManager.getInstance();			
 		manager.removeC0(commentId);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("postId", request.getParameter("postId"));
 		return "redirect:/community/info_community/info_content";
 	}
 }
