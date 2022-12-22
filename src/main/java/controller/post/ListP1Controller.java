84c17ffa0146ddf849c3d938ba081209fe09a76f
정재운 <jaeun101501@gmail.com>
2022-12-22 16:21:34 +0900

    작성글 모아보기 수정 완료

--- src/main/java/controller/post/ListP1Controller.java
+++ src/main/java/controller/post/ListP1Controller.java
@@ -3,8 +3,12 @@ package controller.post;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import controller.Controller;
+import controller.user.UserSessionUtils;
 import model.PostGroup;
+import model.UserInfo;
 import model.service.UserManager;
 
 public class ListP1Controller implements Controller {
@@ -12,10 +16,14 @@ public class ListP1Controller implements Controller {
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
 		
     	UserManager manager = UserManager.getInstance();
+		HttpSession session = request.getSession();	
+
 		List<PostGroup> p1List = manager.findP1List();
-		
+		String loginId = UserSessionUtils.getLoginId(session);
+		UserInfo user = manager.findUser(loginId);
 		// commList 객체를 request에 저장하여 커뮤니티 리스트 화면으로 이동(forwarding)
 		request.setAttribute("p1List", p1List);
+		request.setAttribute("user", user);
 		for(PostGroup pg :p1List) {
 			System.out.println(pg.toString());
 		}
